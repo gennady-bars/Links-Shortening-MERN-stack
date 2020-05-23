50cfb500e73d6ca611ca6def391c22e648fc9705
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-05-23 21:39:53 +0300

    add MongoDB Atlas config

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-const express = require('express')
-const config = require('config')
-const mongoose = require('mongoose')
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
 
-const PORT = config.get('port') || 5000
+const PORT = config.get("port") || 5000;
 
 async function start() {
   try {
-    
+    await mongoose.connect(config.get("mongoUri"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+
+    app.listen(PORT, () => {
+      console.log(`App has been started on port ${PORT}...`);
+    });
   } catch (error) {
-    console.log(`Server Error ${e.message}`);
-    process.exit(1)
+    console.log(`Server Error ${error.message}`);
+    process.exit(1);
   }
 }
 
-
-
-
-
-
-
-
-app.listen(PORT, () => {
-  console.log(`App has been started on port ${PORT}...`);
-})
+start();
