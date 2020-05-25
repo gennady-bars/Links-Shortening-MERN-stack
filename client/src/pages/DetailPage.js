ea0a373d0f48a866d6b4f065655dd973d38a8754
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-05-26 00:27:03 +0300

    add Loader

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState, useCallback, useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useHttp } from "../hooks/http.hook";
+import { AuthContext } from "../context/AuthContext";
+import Loader from '../components/Loader'
 
-const DetailPage = props => {
-    return (
-        <div>
-            <h1>Detail Page</h1>
-        </div>
-    )
-}
+const DetailPage = () => {
+  const { token } = useContext(AuthContext);
+  const { request, loading } = useHttp();
+  const [link, setLink] = useState(null);
+  const linkId = useParams().id;
+
+  const getLink = useCallback(async () => {
+    try {
+      const fetched = await request(`/api/link/${linkId}`, "GET", null, {
+        Authorization: `Bearer ${token}`,
+      });
+
+      setLink(fetched);
+    } catch (error) {}
+  }, [token, linkId, request]);
+
+  useEffect(() => {
+    getLink();
+  }, [getLink]);
+
+  if (loading) {
+      return <Loader/>
+  }
+  return (
+    <div>
+      <h1>Detail Page</h1>
+    </div>
+  );
+};
 
 export default DetailPage;
