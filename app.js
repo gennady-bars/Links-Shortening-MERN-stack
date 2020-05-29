1c56b32f5c9cf6160d50d1c3b69326bf0ef696d5
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-05-29 14:42:02 +0300

    add cross-env

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
+const path = require('path')
 const authRouter = require('./routes/auth.routes')
 const linkRouter = require('./routes/link.routes')
 const redirectRouter = require('./routes/redirect.routes')
@@ -12,6 +13,14 @@ app.use(express.json({extended: true}))
 app.use('/api/auth', authRouter)
 app.use('/api/link', linkRouter)
 app.use('/t', redirectRouter)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = config.get("port") || 5000;
 
